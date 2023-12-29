9cf8e731ff8c580966cbc626e56d399ebd3e244f
zelta <swastik.pandey2011@gmail.com>
2023-12-29 22:19:37 +0530

    feat: added driver code

--- cmd/img-server/main.go
+++ cmd/img-server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/spf13/cobra"
 	"github.com/zelta-7/img-server/pkg/config"
+	"github.com/zelta-7/img-server/pkg/repository"
+	"github.com/zelta-7/img-server/pkg/service"
+	"github.com/zelta-7/img-server/pkg/transport"
 )
 
 func main() {
@@ -25,7 +31,29 @@ func main() {
 		log.Fatalf("Error in executing the server: %v", err)
 		os.Exit(1)
 	}
-	os.Exit(0)
+
+	dbConnectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", cfg.DB.Name, cfg.DB.Port, cfg.DB.Username, cfg.DB.Password, cfg.DB.Name)
+	fmt.Println(dbConnectionString)
+	db, err := gorm.Open("postgres", dbConnectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&repository.ImageRecord{})
+	imgRepo := repository.NewImgRepo(db)
+	log.Print("Repository layer initialised.")
+
+	imgService := service.NewImgService(imgRepo)
+	log.Print("Service layer initialised.")
+
+	r, err := transport.Route(imgService)
+	if err != nil {
+		log.Print("Error creating router")
+		panic(err)
+	}
+
+	r.Run(":8080")
 }
 
 func run(cfg *config.Config) {
